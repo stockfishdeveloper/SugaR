39dd1f21c159da6c0c7949d83f80093ff21478ce
Marco Zerbinati <zerbinati.marco@gmail.com>
2015-04-25 14:23:57 +0200

    union
    Merge branch 'assorted' into union (7660258)
    union
    commit 2647c3135688644aeae65cb2291abf7a2f47c512 2 parents 571af18 +
    a091e4d
    lucasart authored

--- srcS/evaluate.cpp
+++ srcS/evaluate.cpp
@@ -160,17 +160,17 @@ namespace {
   const Score ThreatenedByHangingPawn = S(47, 56);
 
   // Assorted bonuses and penalties used by evaluation
-  const Score KingOnOne          = S( 2, 58);
-  const Score KingOnMany         = S( 6,125);
-  const Score RookOnPawn         = S( 7, 27);
-  const Score RookOnOpenFile     = S(43, 21);
+  const Score KingOnOne          = S( 2, 53);
+  const Score KingOnMany         = S( 7,105);
+  const Score RookOnPawn         = S( 7, 23);
+  const Score RookOnOpenFile     = S(42, 23);
   const Score RookOnSemiOpenFile = S(19, 10);
-  const Score BishopPawns        = S( 8, 12);
-  const Score MinorBehindPawn    = S(16,  0);
-  const Score TrappedRook        = S(92,  0);
-  const Score Unstoppable        = S( 0, 20);
-  const Score Hanging            = S(31, 26);
-  const Score PawnAttackThreat   = S(20, 20);
+  const Score BishopPawns        = S( 8, 14);
+  const Score MinorBehindPawn    = S(17,  0);
+  const Score TrappedRook        = S(83,  0);
+  const Score Unstoppable        = S( 0, 23);
+  const Score Hanging            = S(35, 26);
+  const Score PawnAttackThreat   = S(19, 19);
   const Score PawnSafePush       = S( 5,  5);
 
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
