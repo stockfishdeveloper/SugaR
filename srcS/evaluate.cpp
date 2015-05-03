bf7f39eae9454bd91e7b341cb8381c82a9c5c635
Marco Zerbinati <zerbinati.marco@gmail.com>
2015-05-03 16:27:36 +0200

    Do not give threat bonus if pawn is pinned.
    no_pinned_pawn_threats
    commit 2a70b65c9e12531b1b3a808242e009bbe7ab077d 1 parent cc54a91
    mbootsector authored

--- srcS/evaluate.cpp
+++ srcS/evaluate.cpp
@@ -506,7 +506,7 @@ namespace {
     {
         b = pos.pieces(Us, PAWN) & ( ~ei.attackedBy[Them][ALL_PIECES]
                                     | ei.attackedBy[Us][ALL_PIECES]);
-
+        b &= ~(ei.pinnedPieces[Us] & b);
         safeThreats = (shift_bb<Right>(b) | shift_bb<Left>(b)) & weak;
 
         if (weak ^ safeThreats)
